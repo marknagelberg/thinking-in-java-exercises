ed8b303e3d7930d98ed30cfa12b2ad3947713390
U-PRAINC\nagelberg <nagelberg@WRKSTN132.prainc.local>
2016-12-13 12:50:49 -0600

    Complete Interfaces ex5

--- Interfaces/ex5/Main.java
+++ Interfaces/ex5/Main.java
@@ -1,8 +1,8 @@
-import package2.*;
+import pkg.package2.*;
 
 public class Main {
     public static void main(String[] args) {
-        package2.ClassTwo c2 = new package2.ClassTwo();
+        pkg.package2.ClassTwo c2 = new pkg.package2.ClassTwo();
         c2.method1();
         c2.method2();
         c2.method3();
